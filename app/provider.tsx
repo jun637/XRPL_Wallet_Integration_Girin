2fa157a2724280c2f48ddf78d4d8df2476386765
2wheeh <wonhee@girin.app>
2024-11-11 15:38:23 +0900

    chore: run format

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,6 +1,6 @@
-"use client";
-import React, { ReactNode } from "react";
-import { ClientContextProvider } from "@/context/ClientContext";
+'use client';
+import React, { ReactNode } from 'react';
+import { ClientContextProvider } from '@/context/ClientContext';
 
 export default function Provider({ children }: { children: ReactNode }) {
   return <ClientContextProvider>{children}</ClientContextProvider>;
