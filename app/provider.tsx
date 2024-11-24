904b9831dead47de15270049a7e0c0bc25ebca7e
2wheeh <wonhee@girin.app>
2024-11-24 18:11:08 +0900

    chore: cleanup

--- app/provider.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import React, { ReactNode } from 'react';
-import { ClientContextProvider } from '@/context/ClientContext';
-
-export default function Provider({ children }: { children: ReactNode }) {
-  return <ClientContextProvider>{children}</ClientContextProvider>;
-}
